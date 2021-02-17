02ff4a2d42e9fe53c3cf19a71edc4cc4e5c4260e
andrii.kovalenko <adrekoval@gmail.com>
2021-02-18 00:37:53 +0200

    testing release to test pypi, attempt 6

---
+++ README.md
@@ -0,0 +1,1 @@
+# ScrapingAnt API client for Python
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapingant-client-python",
+    version="0.1.1",
+    author="andrii.kovalenko",
+    author_email="adrekoval@gmail.com",
+    license='Apache-2.0',
+    description="Official python client for the ScrapingAnt API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ScrapingAnt/scrapingant-client-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Browsers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    keywords="scrapingant api scraper scraping",
+    python_requires='~=3.5',
+    install_requires=['requests~=2.*'],
+)
