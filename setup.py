f51f0385205a51beff29a865cc8f1801c3441924
andrii.kovalenko <adrekoval@gmail.com>
2021-02-20 22:24:33 +0200

    feature/issue4-remove-proxy-country-list: bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scrapingant-client",
-    version="0.2.0",
+    version="0.3.0",
     author="andrii.kovalenko",
     author_email="adrekoval@gmail.com",
     license='Apache-2.0',
